de7219a7d713c40992e506dd75405d0a93638797
Nay0ne <nayone606@googlemail.com>
2023-05-11 15:11:15 +0100

    deps instlld, server and routes framworked

---
+++ server.js
@@ -0,0 +1,11 @@
+//  create server
+const express = require("express");
+const app = express();
+const db = require("./db.connection");
+const port = 3000;
+
+app.listen(port, async () => {
+    await db.sync();
+    // add router endpoint once set
+    console.log(`listening at http://localhost:${port}/addrouterendpoint`)
+})
