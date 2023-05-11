10968185cd05e469421af3cdc7112e30c5fb9cf2
Nay0ne <nayone606@googlemail.com>
2023-05-11 20:40:13 +0100

    add shows routes and get

--- src/routes/shows.js
+++ src/routes/shows.js
@@ -1,3 +1,24 @@
 const { Router } = require('express');
-const app = require('../app');
-const router = express.Router();
+const router = Router();
+const Show = require("../../models");
+
+router.get("/", async (req, res, next) => {
+   try {
+        const shows = await Show.findAll();
+        res.json(shows);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const show = await Show.findByPk(id);
+        res.json(show);
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router
