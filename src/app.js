d34bceda4f22760414836ea33a4abfd381dd9130
Nay0ne <nayone606@googlemail.com>
2023-05-11 20:40:28 +0100

    update shows rout into app level

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const users = require("./routes/users");
+const shows = require("./routes/shows");
 app.use("/users", users);
+app.use("/shows", shows);
 const { db } = require('../db/connection');
 app.use(express.json());
