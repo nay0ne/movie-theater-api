3a997cbe2d9e266eb3e15a605c819759db63630a
Nay0ne <nayone606@googlemail.com>
2023-05-11 19:41:57 +0100

    User gets

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,3 +1,23 @@
 const { Router } = require('express');
 const app = require('../app');
 const router = express.Router();
+const User = require("../../models");
+
+router.get("/users", async (req, res, next) => {
+   try {
+        const users = await User.findall();
+        res.json(users);
+    } catch(error) {
+        next(error);
+    }
+});
+
+router.get("/users/:id", async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        user = await Show.findByPk(id);
+        res.json(user);
+    } catch(error) {
+        next(error);
+    }
+});
