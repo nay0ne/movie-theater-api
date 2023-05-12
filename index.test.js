5db41a379815b52f4eac2bf1a3a23e815f348afe
Nay0ne <nayone606@googlemail.com>
2023-05-12 12:08:11 +0100

    add tests for shows

--- index.test.js
+++ index.test.js
@@ -4,5 +4,12 @@ const app = require("./src/app");
 const { it, expect, describe } = require("@jest/globals")
 
 describe("./shows endpoint", () => {
-    it()
+    it("test shows endpoint", async () => {
+        const response = await request(app).get("/shows");
+        expect(response.statusCode).toBe(200);
+        expect(response.body[0]).toHaveProperty("title");
+        expect(response.body[0]).toHaveProperty("genre");
+        expect(response.body[0]).toHaveProperty("rating");
+        expect(response.body[0]).toHaveProperty("status");
+    })
 })
