c0fcf57debd08f7f356192f018f919a3f492b4cc
Nay0ne <nayone606@googlemail.com>
2023-05-12 12:03:10 +0100

    update tests and add jest

---
+++ index.test.js
@@ -0,0 +1,8 @@
+//  endpoint testing
+const request = require("supertest");
+const app = require("./src/app");
+const { it, expect, describe } = require("@jest/globals")
+
+describe("./shows endpoint", () => {
+    it()
+})
