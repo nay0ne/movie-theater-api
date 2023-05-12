fa0c0335f4dd079cd7d9d7b93b41b40444c25215
Nay0ne <nayone606@googlemail.com>
2023-05-12 13:41:26 +0100

    reformatting tests

--- index.test.js
+++ index.test.js
@@ -1,11 +1,22 @@
 //  endpoint testing
 const request = require("supertest");
-const app = require("./src/app");
+const users = require("./src/app");
+const shows = require("./src/app");
 const { it, expect, describe } = require("@jest/globals")
+
+describe("./users endpoint", () => {
+    it("test users endpoint", async () => {
+        const response = await request(users).get("/users");
+        expect(response.statusCode).toBe(200);
+        expect(response.body[0]).toHaveProperty("username");
+        expect(response.body[0]).toHaveProperty("password");
+    })
+})
+
 
 describe("./shows endpoint", () => {
     it("test shows endpoint", async () => {
-        const response = await request(app).get("/shows");
+        const response = await request(shows).get("/shows");
         expect(response.statusCode).toBe(200);
         expect(response.body[0]).toHaveProperty("title");
         expect(response.body[0]).toHaveProperty("genre");
