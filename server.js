988f960618c140345a4da15661d6c99f8f1485aa
Nay0ne <nayone606@googlemail.com>
2023-05-12 10:22:10 +0100

    add seed route into server

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 const app = require("./src/app");
 const { db } = require('./db/connection');
 const port = 3000;
+//  add seed file-
+const seed = require("./seed");
 
 app.listen(port, async () => {
     await db.sync();
     // add router endpoint once set
     console.log(`listening at http://localhost:${port}/addrouterendpoint`)
-})
+});
